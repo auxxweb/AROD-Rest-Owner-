4761e983d2e0e85fa3499b80f4125311ab775ec0
Rahulraj <rahulrohiniraju@gmail.com>
2025-02-01 02:53:20 +0530

    empty

--- src/api/index.js
+++
@@ -1,8 +0,0 @@
-import { createApi } from "@reduxjs/toolkit/query/react";
-import customApiHandler from "./customApiHandler";
-
-export const api = createApi({
-    reducerPath:'quranApi',
-    baseQuery:customApiHandler(),
-    endpoints: () => ({})
-})
